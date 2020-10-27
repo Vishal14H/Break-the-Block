2d5b09db7286c97c207cf47b0cdb0a3e2eb14dc0
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-10-28 01:08:41 +0530

    Math.Clamp Limit

--- Break The Block/Assets/Scripts/Paddle.cs
+++ Break The Block/Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f ;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,10 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log( Input.mousePosition.x / Screen.width * screenWidthInUnits );
+        float mousePosInUnits =( Input.mousePosition.x / Screen.width * screenWidthInUnits );
+        Vector2 paddlePos = new Vector2(transform.position.x , transform.position.y );
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX , maxX);
+        transform.position = paddlePos;
         
     }
 }
