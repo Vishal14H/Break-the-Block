27fb28cfaf4eb8353108fe4e9e5e952c91166417
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-10-30 22:24:58 +0530

    Bouncing the Ball

--- Break The Block/Assets/Scripts/Ball.cs
+++ Break The Block/Assets/Scripts/Ball.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +20,27 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+    
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
-        
+
     }
 }
