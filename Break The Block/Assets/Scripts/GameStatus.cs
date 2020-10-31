8d21e6a1e0f4dd48d78c57552f901c575601f87b
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-10-31 13:40:07 +0530

    Add Player Score

---
+++ Break The Block/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f , 10f)] [SerializeField] float gameSpeed=1f;
+    [SerializeField] int pointPerBlock = 80;
+
+    [SerializeField] int currentScore =0;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointPerBlock;
+    }
+}
