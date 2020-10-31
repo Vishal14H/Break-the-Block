63a0fd2e2cc485a3c5cf95ae3c54e992d4ddc072
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-10-31 23:32:31 +0530

    Implementing Singleton Pattern

--- Break The Block/Assets/Scripts/GameStatus.cs
+++ Break The Block/Assets/Scripts/GameStatus.cs
@@ -1,14 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f , 10f)] [SerializeField] float gameSpeed=1f;
-    [SerializeField] int pointPerBlock = 80;
-
+    [SerializeField] int pointPerBlock = 10;
+    [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] int currentScore =0;
-   
+
+
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if(gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+   public void Start()
+   {
+       scoreText.text = currentScore.ToString();
+
+   }
 
     // Update is called once per frame
     void Update()
@@ -19,5 +42,10 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointPerBlock;
+        scoreText.text = currentScore.ToString();
+    }
+    public void ResetGame()
+    {
+        Destroy(gameObject);
     }
 }
